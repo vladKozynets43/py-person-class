6dc517bf219a6a965bc4b0ad25b1149e8384d524
Vlad <vladk4996@gmail.com>
2024-11-07 14:40:20 +0200

    FIX

--- app/main.py
+++ app/main.py
@@ -4,10 +4,6 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        if "wife" in Person.people:
-            self.wife = None
-        if "husband" in Person.people:
-            self.husband = None
         Person.people[name] = self
 
     @classmethod
@@ -16,7 +12,6 @@ class Person:
 
 
 def create_person_list(people_data: list[dict]) -> list[Person]:
-
     Person.reset_people()
 
     person_instances = [
@@ -25,16 +20,16 @@ def create_person_list(people_data: list[dict]) -> list[Person]:
 
     for data in people_data:
         current_person = Person.people[data["name"]]
-        spouse_role = "wife" if "wife" in data else "husband"
-        spouse_name = data.get(spouse_role)
+        spouse_name = data.get("wife") or data.get("husband")
 
         if spouse_name:
             spouse_instance = Person.people.get(spouse_name)
-            setattr(current_person, spouse_role, spouse_instance)
-
-            if spouse_role == "wife":
-                spouse_instance.husband = current_person
-            elif spouse_role == "husband":
-                spouse_instance.wife = current_person
+            if spouse_instance:
+                if "wife" in data:
+                    current_person.wife = spouse_instance
+                    spouse_instance.husband = current_person
+                elif "husband" in data:
+                    current_person.husband = spouse_instance
+                    spouse_instance.wife = current_person
 
     return person_instances
